16317ea535dea9e46ce4a1f48ebc97462fc23d49
MalerX <malleerx@gmail.com>
2021-04-30 03:11:14 +0300

    Start write GUI. Add Main Window.

--- client/src/main/java/boba/windows/MainWindowCtrl.java
+++ client/src/main/java/boba/windows/MainWindowCtrl.java
@@ -1,11 +1,18 @@
 package boba.windows;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class MainWindowCtrl {
+    public Button backCloud;
     private Stage mainWindow;
 
     public void setMainWindow(Stage mainWindow) {
         this.mainWindow = mainWindow;
+    }
+
+    public void backCloud(ActionEvent actionEvent) {
+        System.out.println("Back");
     }
 }
